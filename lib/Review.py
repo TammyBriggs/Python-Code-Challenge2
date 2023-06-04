7812661f0a34630cfe94ac404e205d7e97178557
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-06-04 16:39:09 +0300

    Code for Initializers, Readers, and Writers in the Review class written

--- lib/Review.py
+++ lib/Review.py
@@ -11,4 +11,23 @@ session = Session()
 Base = declarative_base()
 
 class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    rating = Column(Float)
+
+    customer = relationship('Customer', backref='reviews')
+    restaurant = relationship('Restaurant', backref='reviews')
+
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+
+    def rating(self):
+        return self.rating
     
+    @classmethod
+    def all(cls):
+        return session.query(cls).all()
