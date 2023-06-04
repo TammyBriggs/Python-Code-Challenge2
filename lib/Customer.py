9e9a8c932be2b1c093e2b2b68e0f8a7fe15beeab
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-06-04 16:34:38 +0300

    SQLAlchemy components imported and database engine and session set up.

--- lib/Customer.py
+++ lib/Customer.py
@@ -1,2 +1,34 @@
-class Customer:
-    pass
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Create the SQLAlchemy engine and session
+engine = create_engine('sqlite:///customers.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Base class for declarative models
+Base = declarative_base()
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    given_name = Column(String)
+    family_name = Column(String)
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+
+    def given_name(self):
+        return self.given_name
+    
+    def family_name(self):
+        return self.family_name
+    
+    def full_name(self):
+        return f'{self.given_name} {self.family_name}'
+    
+    @classmethod
+    def all(cls):
+        return session.query(cls).all()
