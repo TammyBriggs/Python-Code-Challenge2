cecc7f84e10403940c43e1dfdeb05e010651b5e6
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-06-04 17:01:09 +0300

    Object Relationship Methods for the Restaurant class written

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 # Create the SQLAlchemy engine and session
 engine = create_engine('sqlite:///restaurants.db')
@@ -15,9 +15,21 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
+    reviews = relationship('Review', back_populates='restaurant')
+    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+
     def __init__(self, name):
         self._name = name
 
     @property
     def name(self):
         return self._name
+    
+    def reviews(self):
+        return self.reviews
+    
+    def customers(self):
+        unique_customers = set()
+        for review in self.reviews:
+            unique_customers.add(review.customer)
+        return list(unique_customers)
